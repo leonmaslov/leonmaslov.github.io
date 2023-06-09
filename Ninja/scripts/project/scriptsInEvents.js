b137a2edcbb2c9abae385243fda8934c629ec710
leonmaslov <136018135+leonmaslov@users.noreply.github.com>
2023-06-09 13:46:49 +0300

    Add files via upload

---
+++ Ninja/scripts/project/scriptsInEvents.js
@@ -0,0 +1,24 @@
+
+
+
+const scriptsInEvents = {
+
+	async Events_game_Event3_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#50770d'
+	},
+
+	async Events_levels_Event5_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#50770d'
+	},
+
+	async Events_home_Event3_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#50770d'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
